1862eda7226a6678770c7f5320644dd814ca4dbf
Terry <terry@terrytilley.com>
2016-07-11 15:55:13 +0100

    Balance method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
-desrcibe Oystercard do
-  
+require 'oystercard'
+
+describe Oystercard do
+
+  describe 'initialize' do
+    subject(:card) {described_class.new}
+
+    it 'check new card has balance 0' do
+      expect(subject.balance).to eq(0)
+    end
+
+  end
+
 end
